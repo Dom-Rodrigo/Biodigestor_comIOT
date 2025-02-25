9d4e04f1f6d7d95f3f0a6e4ead9ba3f1cae2a053
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-02-25 17:11:34 -0300

    Inicializa display

--- Controlador_Biodigestor.c
+++ Controlador_Biodigestor.c
@@ -3,7 +3,10 @@
 #include "hardware/pwm.h"
 #include "pico/bootrom.h"
 #include "hardware/adc.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
 #include "math.h"
+
 
 #define PWM_AGITADOR 20
 
@@ -16,14 +19,19 @@
 #define LED_RED 13
 
 #define VRX_PIN 26   
-#define VRY_PIN 27   
+#define VRY_PIN 27
+
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
 
 uint pwm_setup(int gpio){
     /* 60Hz pro motor e bombas*/
     gpio_set_function(gpio, GPIO_FUNC_PWM);
     uint slice = pwm_gpio_to_slice_num(gpio);
     pwm_set_wrap(slice, 65535);
-    pwm_set_clkdiv(slice, 32); // 442Hz
+    pwm_set_clkdiv(slice, 32); 
     pwm_set_enabled(slice, true);
     return slice;
 }
@@ -81,6 +89,19 @@ bool repeating_timer_callback(struct repeating_timer *t){
 int main()
 {
     stdio_init_all();
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA); // Pull up the data line
+    gpio_pull_up(I2C_SCL); // Pull up the clock line
+    ssd1306_t ssd; // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_fill(&ssd, false); // Limpa display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+    
     adc_init();
 
     uint slice_agitador = pwm_setup(PWM_AGITADOR);
