ed64843ebf2c480a847dc015f85e80e2d9d9f486
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-06-09 00:37:37 -0300

    Conserta bug do adc da temperatura

--- Controlador_Biodigestor.c
+++ Controlador_Biodigestor.c
@@ -549,6 +549,7 @@ static float read_onboard_temperature(const char unit) {
     /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
     const float conversionFactor = 3.3f / (1 << 12);
 
+    adc_select_input(4);
     float adc = (float)adc_read() * conversionFactor;
     float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
 
